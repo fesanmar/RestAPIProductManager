5b86fcf0a4bf97d72e18d3ecb3d061ea554fc6f3
Felipe <fesanmar@gmail.com>
2021-01-18 12:11:41 +0100

    Adds CORS policy.

--- src/main/java/com/felipesantacruz/productmanager/config/AppConfig.java
+++ src/main/java/com/felipesantacruz/productmanager/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.felipesantacruz.productmanager.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig
@@ -11,5 +13,22 @@ public class AppConfig
 	public ModelMapper modelMapper()
 	{
 		return new ModelMapper();
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer()
+	{
+		return new WebMvcConfigurer()
+		{
+			@Override
+			public void addCorsMappings(CorsRegistry registry)
+			{
+				// registry.addMapping("/**"); Allow any origin
+				registry.addMapping("/api/**")
+					.allowedOrigins("http://localhost:8081")
+					.allowedMethods("GET", "POST", "PUT", "DELETE")
+					.maxAge(3600);
+			}
+		};
 	}
 }
