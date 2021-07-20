501afa6d28600dcc79cf14c799498bf2e44fa1c0
Felipe <fesanmar@gmail.com>
2021-07-20 11:58:41 +0200

    Creates Authorization and Resources server

--- src/main/java/com/felipesantacruz/productmanager/security/SecurityConfig.java
+++ src/main/java/com/felipesantacruz/productmanager/security/SecurityConfig.java
@@ -1,35 +1,28 @@
 package com.felipesantacruz.productmanager.security;
 
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@Order(1)
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
-	private static final String USER = "USER";
-	private static final String ADMIN = "ADMIN";
-	private static final String ORDER_URL = "api/order/**";
-	private static final String PRODUCT_URL = "/api/product/**";
-	private final AuthenticationEntryPoint authEntryPoint;
+
 	private final UserDetailsService userDetailsService;
 	private final PasswordEncoder passwordEncoder;
-	private final AccessDeniedHandler accessDeniedHandler;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception
@@ -41,20 +34,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
-		http.httpBasic()
-			.authenticationEntryPoint(authEntryPoint)
+		http.requestMatchers()
+			.antMatchers("/login", "/oauth/authorize")
 			.and()
 			.authorizeRequests()
-				.antMatchers(GET, PRODUCT_URL).hasRole(USER)
-				.antMatchers(POST, PRODUCT_URL).hasRole(ADMIN)
-				.antMatchers(PUT, PRODUCT_URL).hasRole(ADMIN)
-				.antMatchers(DELETE, PRODUCT_URL).hasRole(ADMIN)
-				.antMatchers(POST, ORDER_URL).hasAnyRole(USER,ADMIN)
-				.anyRequest().authenticated()
-			.and()
-			.csrf().disable()
-			.exceptionHandling().accessDeniedHandler(accessDeniedHandler);				
+	        .anyRequest().authenticated()
+	        .and()
+	        .formLogin().permitAll();
+					
 	}
-	
-	
+
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception
+	{
+		return super.authenticationManagerBean();
+	}
 }
---
+++ src/main/java/com/felipesantacruz/productmanager/security/oauth2/OAuth2ResourceServer.java
@@ -0,0 +1,48 @@
+package com.felipesantacruz.productmanager.security.oauth2;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+@EnableResourceServer
+@SuppressWarnings("deprecation")
+public class OAuth2ResourceServer extends ResourceServerConfigurerAdapter
+{
+	
+	private static final String USER = "USER";
+	private static final String ADMIN = "ADMIN";
+	private static final String ORDER_URL = "api/order/**";
+	private static final String PRODUCT_URL = "/api/product/**";
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception
+	{
+		resources.resourceId("oauth2-resource");
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception
+	{
+		http
+			.csrf().disable()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeRequests()
+				.antMatchers(GET, PRODUCT_URL).hasRole(USER)
+				.antMatchers(POST, PRODUCT_URL).hasRole(ADMIN)
+				.antMatchers(PUT, PRODUCT_URL).hasRole(ADMIN)
+				.antMatchers(DELETE, PRODUCT_URL).hasRole(ADMIN)
+				.antMatchers(POST, ORDER_URL).hasAnyRole(USER,ADMIN)
+				.anyRequest().authenticated();
+	}
+}
